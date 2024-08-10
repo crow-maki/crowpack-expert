0f17bf0de47715907b74028ba6277eb41ad413b7
Alic Szecsei <aszecsei@gmail.com>
2024-08-10 01:26:20 -0700

    Lost Cities still doesn't work. Giving up :/

--- kubejs/server_scripts/spawn.js
+++ kubejs/server_scripts/spawn.js
@@ -1,21 +1,6 @@
-let $HeightMapTypes = Java.loadClass('net.minecraft.world.level.levelgen.Heightmap$Types')
-
 PlayerEvents.loggedIn(event => {
   const startingStage = 'starting'
   if (!event.player.stages.has(startingStage)) {
-
-    let lostCitiesLevel = event.server.getLevel("lostcities:lostcity")
-    let playerPos = event.player.blockPosition().mutable()
-    event.server.runCommand(`forge generate ${playerPos.x} 0 ${playerPos.z} 1 lostcities:lostcity`)
-
-    event.server.scheduleInTicks(100, () => {
-      let tpY = lostCitiesLevel.getChunkAt(playerPos).getHeight($HeightMapTypes.WORLD_SURFACE_WG, playerPos.x, playerPos.z)
-      let tpPos = playerPos.setY(tpY)
-      event.player.teleportTo("lostcities:lostcity", tpPos.x, tpPos.y, tpPos.z, 0, 0)
-      event.player.spawnLocation = lostCitiesLevel.getBlock(playerPos.x, tpY + 1, playerPos.z)
-
-    })
-
     event.player.give(tome())
 
     event.player.stages.add(startingStage)
