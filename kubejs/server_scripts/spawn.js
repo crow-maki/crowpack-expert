e8b0ae800905c7bca9eebdf879328ecafcc0669e
Alic Szecsei <aszecsei@gmail.com>
2024-08-10 02:50:54 -0700

    Remove achievements optimizer and give player food + backback at spawn

--- kubejs/server_scripts/spawn.js
+++ kubejs/server_scripts/spawn.js
@@ -2,6 +2,8 @@ PlayerEvents.loggedIn(event => {
   const startingStage = 'starting'
   if (!event.player.stages.has(startingStage)) {
     event.player.give(tome())
+    event.player.give('sophisticatedbackpacks:backpack')
+    event.player.give('croptopia:grilled_cheese', 20)
 
     event.player.stages.add(startingStage)
   }
