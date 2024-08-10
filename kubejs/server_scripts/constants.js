c77202dbb8139666399f4d82bf3f192bf6f78eb6
Alic Szecsei <aszecsei@gmail.com>
2024-08-10 00:47:14 -0700

    Wrong tome code

--- kubejs/server_scripts/constants.js
+++ kubejs/server_scripts/constants.js
@@ -1,118 +1,209 @@
 let tome = () => Item.of('eccentrictome:tome', {
-    'eccentrictome:mods': {
+    "eccentrictome:mods": {
+        ad_astra: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "ad_astra:astrodux"
+                }
+            }
+        },
+        advancedperipherals: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "advancedperipherals:manual"
+                }
+            }
+        },
         ae2: {
             0: {
                 Count: 1,
-                id: 'ae2:guide'
+                id: "ae2:guide"
             }
         },
         apotheosis: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'apotheosis:apoth_chronicle' }
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "apotheosis:apoth_chronicle"
+                }
             }
         },
         ars_nouveau: {
             0: {
                 Count: 1,
-                id: 'ars_nouveau:worn_notebook'
+                id: "ars_nouveau:worn_notebook"
             }
         },
-        engineersdecor: {
+        botania: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'engineersdecor:engineersdecor_manual' }
+                id: "botania:lexicon"
+            }
+        },
+        buildinggadgets2: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "buildinggadgets2:buildinggadgets2book"
+                }
+            }
+        },
+        croptopia: {
+            0: {
+                Count: 1,
+                id: "croptopia:guide"
+            }
+        },
+        eidolon: {
+            0: {
+                Count: 1,
+                id: "eidolon:codex"
+            }
+        },
+        enderio: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "enderio:guide"
+                }
+            }
+        },
+        extendedcrafting: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "extendedcrafting:guide"
+                }
+            }
+        },
+        forcecraft: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "forcecraft:force_and_you"
+                }
             }
         },
         ftbquests: {
             0: {
                 Count: 1,
-                id: 'ftbquests:book'
+                id: "ftbquests:book"
             }
         },
-        immersiveengineering: {
+        integrateddynamics: {
             0: {
                 Count: 1,
-                id: 'immersiveengineering:manual'
+                id: "integrateddynamics:on_the_dynamics_of_integration"
             }
         },
-        industrialforegoing: {
+        irons_spellbooks: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'industrialforegoing:industrial_foregoing' }
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "irons_spellbooks:iss_guide_book"
+                }
             }
         },
-        littlelogistics: {
+        laserio: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'littlelogistics:guide' }
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "laserio:laseriobook"
+                }
             }
         },
-        modonomicon: {
+        malum: {
             0: {
                 Count: 1,
-                id: 'modonomicon:modonomicon',
-                tag: { 'modonomicon:book_id': 'theurgy:the_hermetica' }
-            }
-        },
-        modularrouters: {
-            0: {
-                Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'modularrouters:book' }
+                id: "malum:encyclopedia_arcana"
             }
         },
         naturesaura: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'naturesaura:book' }
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "naturesaura:book"
+                }
+            }
+        },
+        nuclearcraft: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "nuclearcraft:nuclearcraft"
+                }
             }
         },
         occultism: {
             0: {
                 Count: 1,
-                id: 'occultism:dictionary_of_spirits',
-                tag: { 'modonomicon:book_id': 'occultism:dictionary_of_spirits' }
+                id: "occultism:dictionary_of_spirits",
+                tag: {
+                    "modonomicon:book_id": "occultism:dictionary_of_spirits"
+                }
             }
         },
         pneumaticcraft: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'pneumaticcraft:book' }
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "pneumaticcraft:book"
+                }
             }
         },
-        powah: {
+        productivebees: {
             0: {
                 Count: 1,
-                id: 'powah:book'
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "productivebees:guide"
+                }
+            }
+        },
+        reactive: {
+            0: {
+                Count: 1,
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "reactive:journal"
+                }
             }
         },
         rftoolsbase: {
             0: {
                 Count: 1,
-                id: 'rftoolsbase:manual'
+                id: "rftoolsbase:manual"
             }
         },
-        sushigocrafting: {
+        solapplepie: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'sushigocrafting:sushigocrafting' }
+                id: "solapplepie:food_book"
             }
         },
         thermal: {
             0: {
                 Count: 1,
-                id: 'patchouli:guide_book',
-                tag: { 'patchouli:book': 'thermal:guidebook' }
+                id: "patchouli:guide_book",
+                tag: {
+                    "patchouli:book": "thermal:guidebook"
+                }
             }
         }
     },
-    'eccentrictome:version': 1.0
+    "eccentrictome:version": 1
 })
